9129e69ecbc3158a72e3d7c0cf9adfdcff73ff83
wchan2 <willchan2@gmail.com>
2016-03-30 17:14:45 -0400

    added an integration test for watching folders and its nested files

---
+++ integration_test.go
@@ -0,0 +1,59 @@
+package fswatch_test
+
+import (
+	"log"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/wchan2/fswatch"
+)
+
+var _ = Describe(`FileSystemWatcher`, func() {
+	Context(`when watching any file under any folder`, func() {
+		var (
+			fswatcher *fswatch.FileSystemWatcher
+			eventQ    chan fswatch.Event
+		)
+
+		BeforeEach(func() {
+			eventQ = make(chan fswatch.Event)
+			fswatcher = fswatch.NewFileSystemWatcher(
+				[]string{"**/*"},
+				eventQ,
+			)
+		})
+
+		JustBeforeEach(func() {
+			go fswatcher.Start()
+
+			// create folders and file here
+			if err := os.Mkdir("testing", os.ModePerm); err != nil {
+				log.Fatal(err.Error())
+			}
+			if f, err := os.Create("testing/test_file.txt"); err != nil {
+				log.Fatal(err.Error())
+			} else {
+				f.Chmod(os.ModePerm)
+				f.Close()
+			}
+		})
+
+		AfterEach(func() {
+			fswatcher.Stop()
+
+			if err := os.RemoveAll("testing"); err != nil {
+				log.Fatal(err.Error())
+			}
+		})
+
+		It(`receives an event for a created file`, func() {
+			Expect(<-eventQ).To(Equal(fswatch.Event{
+				Type: "CHANGED",
+				Data: map[string][]string{
+					"changed": {"./testing/test_file.txt"},
+				},
+			}))
+		})
+	})
+})
