4f79ae89fd9f013e115de9f6dc29866c79012497
William Chan <willchan2@gmail.com>
2016-04-16 22:09:09 -0400

    adding an integration test with listening to all files under a specific directory (recursive)

--- integration_test.go
+++ integration_test.go
@@ -52,4 +52,47 @@ var _ = Describe(`FileSystemWatcher`, func() {
 			}))
 		})
 	})
+
+	Context(`when watching a file under a specific folder`, func() {
+		var (
+			fswatcher *fswatch.FileSystemWatcher
+			eventQ    chan fswatch.Event
+		)
+
+		BeforeEach(func() {
+			eventQ = make(chan fswatch.Event)
+			fswatcher = fswatch.NewFileSystemWatcher(
+				[]string{"testing/**/*"},
+				eventQ,
+			)
+		})
+
+		JustBeforeEach(func() {
+			go fswatcher.Run()
+
+			if err := os.Mkdir("testing", os.ModePerm); err != nil {
+				log.Fatal(err.Error())
+			}
+			if f, err := os.Create("testing/test_file.txt"); err != nil {
+				log.Fatal(err.Error())
+			} else {
+				f.Chmod(os.ModePerm)
+				f.Close()
+			}
+		})
+
+		AfterEach(func() {
+			fswatcher.Stop()
+
+			if err := os.RemoveAll("testing"); err != nil {
+				log.Fatal(err.Error())
+			}
+		})
+
+		It(`receives an event for a created file`, func() {
+			Expect(<-eventQ).To(Equal(fswatch.Event{
+				FilesCreated: []string{"testing/test_file.txt"},
+			}))
+		})
+	})
 })
